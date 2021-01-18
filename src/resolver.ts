c95a038cc4a64b8031b4e16c80ebf26839ab0854
Deeptiman Panda <deeptimanpanda@Deeptimans-Mac-mini.local>
2021-01-18 19:32:10 +0530

    Added Mutation

--- src/resolver.ts
+++ src/resolver.ts
@@ -1,9 +1,14 @@
-import { Query, Resolver } from "type-graphql";
+import { Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class Resolve {
     @Query(() => String)
     hello() {
         return "Hi";
+    }
+
+    @Mutation(() => String)
+    create() {
+        return "Mutation Completed"
     }
 };
